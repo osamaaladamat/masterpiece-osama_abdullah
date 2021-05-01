b810c14686e85cd58684f303988c18e7696bc081
osamaaladamat <osama.ala3damat@gmail.com>
2021-05-01 03:02:10 +0300

    New Projects

---
+++ source code/Spacejobs/resources/views/admin/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('content')
+
+        <div class="container">
+         
+
+        <div class="row">
+        <div class="col-md-4">
+          @include('admin.left-menu')
+        </div>
+        
+    </div>
+</div>
+
+
+@endsection
---
+++ source code/Spacejobs/resources/views/admin/left-menu.blade.php
@@ -0,0 +1,13 @@
+			<div class="card">
+				<div class="card-header">
+					Menu
+				</div>
+				<div class="card-body">
+					<ul class="list-group">
+						<li class="list-group-item"><a href="/dashboard">Home</a></li>
+						<li class="list-group-item"><a href="/dashboard/jobs">View Jobs</a></li>
+				
+						
+					</ul>
+				</div>
+			</div>
